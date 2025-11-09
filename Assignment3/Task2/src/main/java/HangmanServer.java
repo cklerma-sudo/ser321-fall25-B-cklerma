60374f7ee518ff75ad2683a61e3fd9b5aa524ffa
cklerma-sudo <cklerma@asu.edu>
2025-11-08 18:08:32 -0800

    Implement HangmanServer.java

--- Assignment3/Task2/src/main/java/HangmanServer.java
+++ Assignment3/Task2/src/main/java/HangmanServer.java
@@ -1,3 +1,4 @@
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -116,7 +117,29 @@ public class HangmanServer {
                         writeOut(res);
                         connected = false;
                         continue;
-                    } else {
+                    }
+                    else if (type.equals("start")) {
+                        res = startGame(req);
+                    }
+                    else if (type.equals("guessLetter")) {
+                        res = guessLetter(req);
+                    }
+                    else if (type.equals("guessWord")) {
+                        res = guessWord(req);
+                    }
+                    else if (type.equals("state")) {
+                        res = state(req);
+                    }
+                    else if (type.equals("letters")) {
+                        res = lettersGuessed(req);
+                    }
+                    else if (type.equals("leaderboard")) {
+                        res = showLeaderboard(req);
+                    }
+                    else if (type.equals("return")) {
+                        res = returnToMenu(req);
+                    }
+                    else {
                         res = wrongType(req);
                     }
                     writeOut(res);
@@ -128,6 +151,304 @@ public class HangmanServer {
             overandout();
         }
     }
+
+    static JSONObject returnToMenu(JSONObject req) {
+        JSONObject res = new JSONObject();
+        String temp = playerName;
+        resetGame();
+        playerName = temp;
+        res.put("ok", true);
+        res.put("type", "return");
+        res.put("message", "Success, going back to main menu");
+        return res;
+    }
+
+    static JSONObject showLeaderboard(JSONObject req) {
+        JSONObject res = new JSONObject();
+        JSONArray leaderboardRes = new JSONArray();
+        if (leaderboard.isEmpty()) {
+            res.put("ok", true);
+            res.put("count", 0);
+            res.put("type", "leaderboard");
+        }
+        else {
+            int rank = 1;
+            for (Map<String, Object> obj : leaderboard) {
+                JSONObject row = new JSONObject();
+                row.put("rank", rank++);
+                row.put("name", obj.get("name"));
+                row.put("score", obj.get("score"));
+                row.put("difficulty", obj.get("difficulty"));
+                leaderboardRes.put(row);
+            }
+
+            res.put("leaderboard", leaderboardRes);
+            res.put("ok", true);
+            res.put("count", leaderboard.size());
+            res.put("type", "leaderboard");
+        }
+        return res;
+    }
+
+    static JSONObject lettersGuessed(JSONObject req) {
+        JSONObject res = new JSONObject();
+        res.put("ok", true);
+        res.put("type", "letters");
+        JSONArray letters = new JSONArray();
+        for (Character c : guessedLetters) letters.put(c.toString().toUpperCase());
+        res.put("lettersGuessed", letters);
+        res.put("total", guessedLetters.size());
+        return res;
+    }
+
+    static JSONObject state(JSONObject req) {
+        JSONObject res = new JSONObject();
+        res.put("ok", true);
+        res.put("type", "state");
+        res.put("hangStage", HANGMAN_STAGES[wrongGuesses]);
+        res.put("score", score);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < currentWord.length(); i++) {
+            char c = currentWord.charAt(i);
+            if (guessedLetters.contains(c)) {
+                String spaceAdded =" " + c;
+                sb.append(spaceAdded);
+            }
+            else {
+                sb.append(" _");
+            }
+        }
+        String word = sb.toString();
+        res.put("word", word);
+        String numGuess = Integer.toString(wrongGuesses) + "/9";
+        res.put("guesses", numGuess);
+        res.put("lettersAmount", guessedLetters.size());
+        return res;
+    }
+
+    static boolean checkLetter(String word) {
+        boolean allLetters = true;
+        for (int i = 0; i < word.length(); i++) {
+            if (!Character.isLetter(word.charAt(i))) allLetters = false;
+        }
+        return allLetters;
+    }
+
+    static JSONObject guessWord(JSONObject req) {
+        JSONObject res = new JSONObject();
+        String guess = req.getString("letter");
+        if (guess == null) {
+            res.put("ok", false);
+            res.put("message", "Guess cannot be empty");
+            return res;
+        }
+        else if (!checkLetter(guess)) {
+            res.put("ok", false);
+            res.put("message", "Guess must be a word with only letters such as 'rotten'");
+            return res;
+        }
+        else if (guess.length() != currentWord.length()) {
+            res.put("ok", false);
+            res.put("message", "Guess was not the correct amount of letters");
+            return res;
+        }
+        guess = guess.toLowerCase();
+        if (guess.equals(currentWord)) {
+            res.put("loss", false);
+            res.put("ok", true);
+            res.put("type", "guessWord");
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < guess.length(); i++) {
+                sb.append(" ");
+                sb.append(guess.charAt(i));
+            }
+            res.put("word", sb.toString());
+            res.put("won", true);
+            res.put("guess", "correct");
+            int tempScore = currentWord.length()*10;
+            for (int j = 0; j < currentWord.length(); j++) {
+                char c = currentWord.charAt(j);
+                if (guessedLetters.contains(c)) tempScore -= 10;
+            }
+            score += tempScore + 50;
+            if (wrongGuesses == 0) score += 20;
+            res.put("score", score);
+            res.put("hangStage", HANGMAN_STAGES[wrongGuesses]);
+            updateLeaderboard(playerName, score, difficulty);
+            String temp = playerName;
+            resetGame();
+            playerName = temp;
+        }
+        else {
+            res.put("ok", true);
+            res.put("type", "guessWord");
+            res.put("won", false);
+            res.put("score", score);
+            res.put("guess", "incorrect");
+            wrongGuesses++;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < currentWord.length(); i++) {
+                char c = currentWord.charAt(i);
+                if (guessedLetters.contains(c)) {
+                    String spaceAdded =" " + c;
+                    sb.append(spaceAdded);
+                }
+                else {
+                    sb.append(" _");
+                }
+            }
+            String word = sb.toString();
+            res.put("word", word);
+            if (wrongGuesses == 9){
+                res.put("loss", true);
+                res.put("hangStage", HANGMAN_STAGES[wrongGuesses]);
+                res.put("finalWord", currentWord);
+                updateLeaderboard(playerName, score, difficulty);
+                String temp = playerName;
+                resetGame();
+                playerName = temp;
+            }
+            else{
+                res.put("loss", false);
+                res.put("hangStage", HANGMAN_STAGES[wrongGuesses]);
+            }
+        }
+
+        return res;
+    }
+
+    static JSONObject guessLetter(JSONObject req) {
+        JSONObject res = new JSONObject();
+        String guess = req.getString("letter");
+        if (guess == null) {
+            res.put("ok", false);
+            res.put("message", "Guess cannot be empty");
+            return res;
+        }
+        else if (!Character.isLetter(guess.charAt(0))) {
+            res.put("ok", false);
+            res.put("message", "Guess must be a letter such as 'a'");
+            return res;
+        }
+        else if (guessedLetters.contains(guess.charAt(0))) {
+            res.put("ok", false);
+            res.put("message", "You already guessed this letter");
+            return res;
+        }
+        guess = guess.toLowerCase();
+        guessedLetters.add(guess.charAt(0));
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < currentWord.length(); i++) {
+            char c = currentWord.charAt(i);
+            if (guessedLetters.contains(c)) {
+                String spaceAdded =" " + c;
+                sb.append(spaceAdded);
+            }
+            else {
+                sb.append(" _");
+            }
+        }
+        String word = sb.toString();
+        res.put("ok", true);
+        res.put("type", "guessLetter");
+        res.put("word", word);
+        if (currentWord.contains(guess)) {
+            res.put("loss", false);
+            int count = 0;
+            for (int i = 0; i < currentWord.length(); i++) {
+                if (currentWord.charAt(i) == guess.charAt(0)) {
+                    count++;
+                }
+
+            }
+            score += 10*count;
+            if (!word.contains("_")) {
+                res.put("won", true);
+                score += 50;
+                if (wrongGuesses == 0) score += 20;
+                updateLeaderboard(playerName, score, difficulty);
+                res.put("score", score);
+                res.put("hangStage", HANGMAN_STAGES[wrongGuesses]);
+                String temp = playerName;
+                resetGame();
+                playerName = temp;
+            }
+            else{
+                res.put("won", false);
+                res.put("score", score);
+                res.put("hangStage", HANGMAN_STAGES[wrongGuesses]);
+            }
+            res.put("guess", "correct");
+        }
+        else{
+            res.put("won", false);
+            if (score >= 5) score -= 5;
+            res.put("score", score);
+            res.put("guess", "incorrect");
+            wrongGuesses++;
+            if (wrongGuesses == 9){
+                res.put("loss", true);
+                res.put("hangStage", HANGMAN_STAGES[wrongGuesses]);
+                res.put("finalWord", currentWord);
+                updateLeaderboard(playerName, score, difficulty);
+                String temp = playerName;
+                resetGame();
+                playerName = temp;
+            }
+            else{
+                res.put("loss", false);
+                res.put("hangStage", HANGMAN_STAGES[wrongGuesses]);
+            }
+        }
+        return res;
+    }
+
+    static void updateLeaderboard(String name, int score, String difficulty) {
+        Map<String, Object> entry = new HashMap<>();
+        entry.put("name", name);
+        entry.put("score", score);
+        entry.put("difficulty", difficulty);
+        leaderboard.add(entry);
+        leaderboard.sort((a, b) ->
+                Integer.compare(((Number) b.get("score")).intValue(), ((Number) a.get("score")).intValue()));
+        if (leaderboard.size() > 10) leaderboard.removeLast();
+    }
+
+    static JSONObject startGame(JSONObject req) {
+        JSONObject res = new JSONObject();
+        Random rand = new Random();
+        difficulty = req.getString("difficulty");
+        if (!(difficulty.equals("easy") || difficulty.equals("medium") || difficulty.equals("hard"))) {
+            res.put("ok", false);
+            res.put("message", "Difficulty must be easy, medium, or hard");
+            return res;
+        }
+        else if (difficulty.equals("easy")) {
+            int wordNum = rand.nextInt(12);
+            currentWord = EASY_WORDS[wordNum];
+        }
+        else if (difficulty.equals("medium")) {
+            int wordNum = rand.nextInt(14);
+            currentWord = MEDIUM_WORDS[wordNum];
+        }
+        else {
+            int wordNum = rand.nextInt(14);
+            currentWord = HARD_WORDS[wordNum];
+        }
+        String codedWord = " _".repeat(currentWord.length());
+        gameActive = true;
+
+        res.put("ok", true);
+        res.put("word", codedWord);
+        res.put("length", currentWord.length());
+        res.put("difficulty", difficulty);
+        res.put("type", "start");
+        res.put("hangStage", HANGMAN_STAGES[0]);
+        System.out.println("The word is " + currentWord);
+        return res;
+    }
+
+
 
     /**
      * EXAMPLE IMPLEMENTATION: Set player name
