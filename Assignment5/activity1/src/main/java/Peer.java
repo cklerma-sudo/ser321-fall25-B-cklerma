9db6f565944445daacf4b4ddf72f9da3f00e919f
cklerma-sudo <cklerma@asu.edu>
2025-11-24 17:05:28 -0800

    Implement Peer.java

--- Assignment5/activity1/src/main/java/Peer.java
+++ Assignment5/activity1/src/main/java/Peer.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 /**
@@ -24,57 +25,35 @@ public class Peer {
 	/**
 	 * Main method saying hi and also starting the Server thread where other peers can subscribe to listen
 	 *
-	 * @param args[0] username
-	 * @param args[1] port for server
+	 *  args[0] username
+	 *  args[1] port for server
+	 *  args[2] port for peer connection
 	 */
-	public static void main (String[] args) throws Exception {
-
+	public static void main(String[] args) throws Exception {
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 		String username = args[0];
 		System.out.println("Hello " + username + " and welcome! Your port will be " + args[1]);
 
-		// starting the Server Thread, which waits for other peers to want to connect
 		ServerThread serverThread = new ServerThread(args[1]);
 		serverThread.start();
-		Peer peer = new Peer(bufferedReader, args[0], serverThread);
-		peer.updateListenToPeers();
-	}
-	
-	/**
-	 * User is asked to define who they want to subscribe/listen to
-	 * Per default we listen to no one
-	 *
-	 */
-	public void updateListenToPeers() throws Exception {
-		System.out.println("> Who do you want to listen to? Enter host:port");
-		String input = bufferedReader.readLine();
-		String[] setupValue = input.split(" ");
-		for (int i = 0; i < setupValue.length; i++) {
-			String[] address = setupValue[i].split(":");
-			Socket socket = null;
-			try {
-				socket = new Socket(address[0], Integer.valueOf(address[1]));
-				new ClientThread(socket).start();
-			} catch (Exception c) {
-				if (socket != null) {
-					socket.close();
-				} else {
-					System.out.println("Cannot connect, wrong input");
-					System.out.println("Exiting: I know really user friendly");
-					System.exit(0);
-				}
-			}
+
+		Peer peer = new Peer(bufferedReader, username, serverThread);
+
+		if (args.length == 3) {
+			int bootstrapPort = Integer.parseInt(args[2]);
+			serverThread.connectToPeer(bootstrapPort);
 		}
 
-		askForInput();
+		peer.askForInput();
 	}
-	
+
+
 	/**
 	 * Client waits for user to input their message or quit
 	 *
-	 * @param bufReader bufferedReader to listen for user entries
-	 * @param username name of this peer
-	 * @param serverThread server thread that is waiting for peers to sign up
+	 *  bufReader bufferedReader to listen for user entries
+	 *  username name of this peer
+	 *  serverThread server thread that is waiting for peers to sign up
 	 */
 	public void askForInput() throws Exception {
 		try {
@@ -85,7 +64,6 @@ public class Peer {
 					System.out.println("bye, see you next time");
 					break;
 				} else {
-					// we are sending the message to our server thread. this one is then responsible for sending it to listening peers
 					serverThread.sendMessage("{'username': '"+ username +"','message':'" + message + "'}");
 				}	
 			}
